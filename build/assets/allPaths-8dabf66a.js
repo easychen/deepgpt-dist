3eddef7bbd3f66b61d1d3c1c38c36c26746b09ce
Easy <easychen@qq.com>
2023-07-06 17:14:44 +0800

    更新到1.0.6

---
+++ build/assets/allPaths-8dabf66a.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-5aa9ee64.js";import{I as e}from"./index-c332d9fb.js";import{p as r,I as s}from"./index-66a97aab.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
